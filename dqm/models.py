d32eb531cdd8d45b2b2cc661cab294d9a80fb887
stibi93 <seres.tibor@hiflylabs.hu>
2021-04-21 10:56:17 +0200

    Starting project: Flask base implemented, login, registration, authentication, homepage all set up

---
+++ dqm/models.py
@@ -0,0 +1,29 @@
+import flask_bcrypt
+
+from dqm import db, login_manager
+from dqm import bcryt
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), unique=True, nullable=False)
+    email_address = db.Column(db.String(length=50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+
+    @property
+    def password(self):
+        return self.password
+
+    @password.setter
+    def password(self, plain_text_password):
+        self.password_hash = flask_bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+
+    def check_password_correction(self, attempted_password):
+        return bcryt.check_password_hash(self.password_hash, attempted_password)
+
+    def __repr__(self):
+        return f'User  {self.username}'
